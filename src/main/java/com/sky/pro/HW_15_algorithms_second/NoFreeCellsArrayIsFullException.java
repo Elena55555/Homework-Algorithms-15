07952bbe359f8b816e7d73afd23b3f8b3a138d14
Elena <elenela@yandex.ru>
2023-07-21 00:58:42 +0300

    ДЗ 15

--- src/main/java/com/sky/pro/HW_15_algorithms_second/NoFreeCellsArrayIsFullException.java
+++ src/main/java/com/sky/pro/HW_15_algorithms_second/NoFreeCellsArrayIsFullException.java
@@ -1,26 +1,14 @@
 package com.sky.pro.HW_15_algorithms_second;
-
-
 public class NoFreeCellsArrayIsFullException extends RuntimeException{
-
     public NoFreeCellsArrayIsFullException() {
     }
-
-
     public NoFreeCellsArrayIsFullException(String message) {
-
         super(message);
     }
-
-
     public NoFreeCellsArrayIsFullException(String message, Throwable cause) {
-
         super(message, cause);
     }
-
-
     public NoFreeCellsArrayIsFullException(Throwable cause) {
-
         super(cause);
     }
 }
